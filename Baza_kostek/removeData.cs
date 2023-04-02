b1680e88de470553d48c6aaa6cecfc730fa83d82
Piotr Bauer <s210831@sggw.edu.pl>
2023-04-02 15:05:26 +0200

    initial commit

---
+++ Baza_kostek/removeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza_kostek
+{
+    partial class Program
+    {
+        public static void removeData(List<Kostki> wszystkieKostki)
+        {
+
+        }
+    }
+}
