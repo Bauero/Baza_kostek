5f31893a31c13b86f28d3bb014de9e9238b2380a
Piotr Bauer <s210831@sggw.edu.pl>
2023-04-02 15:48:01 +0200

    Usuwanie po nazwie
    Dodanie możliwości usuwania koski po nazwie, z uwzględnieniem pomyłki użytkownika, oraz zmieniłem plik kostki by umożliwić odczyn nazwy kostki poprzez publiczną zmienną "Nazwa"

--- Baza_kostek/removeData.cs
+++ Baza_kostek/removeData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace Baza_kostek
     {
         public static void removeData(List<Kostki> wszystkieKostki)
         {
+            informacje:
+
+            Console.WriteLine("Wybór sposobu usuwania: ");
+            Console.WriteLine("1 - Nazwa kostki");
+            Console.WriteLine("2 - numer koski na liscie");
+            Console.WriteLine("3 - anuluj wpisywanie");
+            Console.Write(">>> ");
+            string ans = Console.ReadLine().Trim();
+
+            switch (ans)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    goto informacje;
+            }
 
         }
     }
